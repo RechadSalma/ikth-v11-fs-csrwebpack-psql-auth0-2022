cfe9a8bccac0dc7ae3eb68b0416d06817777832e
RechadSalma <rechadsalma123@gmail.com>
2022-03-08 15:35:33 +0000

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(morgan("dev"));
@@ -11,18 +11,22 @@ app.use(express.urlencoded({ extended: true }));
 //   origin: "http://localhost:9000",
 //   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // };
-// const whitelist = ["http://localhost:9000", "http://localhost:3000"];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+const whitelist = [
+  "http://localhost:9000",
+  "http://localhost:3000",
+  "https://ikth-v11-backend.herokuapp.com/",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 //routes
 app.get("/", (req, res) => {

9b17307d05f7cea91046ce6bcfc5f79a2fb65e83
RechadSalma <rechadsalma123@gmail.com>
2022-03-08 15:54:16 +0000

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,22 +11,27 @@ app.use(express.urlencoded({ extended: true }));
 //   origin: "http://localhost:9000",
 //   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // };
-const whitelist = [
-  // "http://localhost:9000",
-  // "http://localhost:3000",
-  "https://ikth-v11-frontend.herokuapp.com/",
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+// const whitelist = [
+// "http://localhost:9000",
+// "http://localhost:3000",
+//   "https://ikth-v11-frontend.herokuapp.com/",
+// ];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
+var corsOptions = {
+  origin: "https://ikth-v11-frontend.herokuapp.com/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //routes
 app.get("/", (req, res) => {
@@ -35,7 +40,7 @@ app.get("/", (req, res) => {
 
 const data = { iKname: "rechad kheerdali", dob: 19441111 };
 
-app.get("/api", (req, res) => {
+app.get("/api", cors(corsOptions), (req, res) => {
   res.json(data).status(200);
 });
 

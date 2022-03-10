a48e43b7d64a70adf4d0c9725cb700ad4a066a3f
RechadSalma <rechadsalma123@gmail.com>
2022-03-10 17:42:53 +0000

    TD: trying to connect heroku psql & create table

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -7,12 +7,19 @@ const { Pool } = require("pg");
 //   port: 3211,
 // })
 
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
